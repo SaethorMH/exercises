fdc4846bd446d8c0d360070e605b7cfe0fe40da9
SaethorMH <smhspilari@gmail.com>
2026-08-24 20:19:44 +0200

    Add speed control functionality to bar chart generator

--- Uge2/bar_chart_starter_kit/script.js
+++ Uge2/bar_chart_starter_kit/script.js
@@ -2,11 +2,13 @@ import { rNumber } from "../../utils_lib/utils_lib.js";
 
 const list = document.querySelector("ul");
 const listArray = [];
+let speed = 1000;
+let amount = 20;
 
-setInterval(genColumn, 1000);
+let intervallet = setInterval(genColumn, speed);
 
 function genColumn() {
-  if (listArray.length > 19) {
+  if (listArray.length > amount) {
     listArray.shift();
     list.removeChild(list.firstElementChild);
   }
@@ -17,17 +19,25 @@ function genColumn() {
   const li = document.createElement("li");
   li.style.setProperty("--height", gcNumber);
   list.appendChild(li);
-  console.log("column generated");
+  //   console.log("column generated");
 
   generateFromArray();
 }
 
 function generateFromArray() {
   document.querySelector("ol").innerHTML = "";
-  console.log("AAAHH");
+  //   console.log("AAAHH");
 
   listArray.forEach((element) => {
     document.querySelector("ol").innerHTML +=
       `<li style="--height:${element}">`;
   });
+}
+
+document.querySelector("input").addEventListener("change", changeSpeed);
+function changeSpeed() {
+  speed = 1000 / Number(document.querySelector("input").value);
+  console.log("speed is " + speed);
+  clearInterval(intervallet);
+  intervallet = setInterval(genColumn, speed);
 }
